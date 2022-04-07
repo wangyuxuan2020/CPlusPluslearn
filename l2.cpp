f502a25c20d5f81ceb79abbc3e1450dfb0d53d2f
wangyuxuan2020 <102598771+wangyuxuan2020@users.noreply.github.com>
2022-04-07 20:46:36 +0800

    两个值交换

---
+++ l2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+//½»»»a¡¢bµÄÖµ 
+int main()
+{
+	int A=18,B=45;
+	int C;
+	C=A;
+	A=B;
+	B=C;
+	printf("A= %d,B= %d \n",A,B);
+	    
+	int a,b,c;
+    a=12;	
+	b=34;
+	c=a+b;
+	cout<<c;
+	
+	return 0;
+}
