74fe561805d1960eb217921b6b8ef9d8fd1753ca
wangyuxuan2020 <102598771+wangyuxuan2020@users.noreply.github.com>
2022-04-12 21:24:45 +0800

    鸡兔同笼的算法

---
+++ l5.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{	
+	int j,t,h,f;
+	h=30;f=90;
+	j=(4*h-f)/2;
+	t=h-j;
+	cout<<"ผฆ:"<<j<<endl; 
+	cout<<"อร:"<<t<<endl; 
+	return 0; 
+}
+
+
+
