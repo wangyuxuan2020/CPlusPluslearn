446bfe1c33277dfbd222a8a996d030cbfacb040d
wangyuxuan2020 <102598771+wangyuxuan2020@users.noreply.github.com>
2022-05-01 13:53:51 +0800

    判断两数大小

---
+++ l11.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	
+	cin>>a>>b;
+	
+	if(a>b)
+	{
+		cout<<a<<">"<<b;
+	}
+	else
+	{
+		if(a<b)
+		{
+			cout<<a<<"<"<<b;
+		}
+		else
+		{
+		cout<<a<<"="<<b;
+		}
+	}
+	
+	return 0;
+}
