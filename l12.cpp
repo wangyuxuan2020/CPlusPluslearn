4808332b959d6f4b288d086ae1e03eae2d231393
wangyuxuan2020 <102598771+wangyuxuan2020@users.noreply.github.com>
2022-05-01 13:54:44 +0800

    判断数字是否可以整除2或5

---
+++ l12.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,c,d;
+	cin>>a;
+	c=a%2;
+	d=a%5;
+	if(d==0||c==0)
+	{
+		cout<<"ÊÇ";
+	
+	}
+	else
+	{
+		cout<<"·ñ";
+		
+	}
+	return 0;
+}
+	
