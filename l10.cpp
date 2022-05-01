8edf5714adc85e12f8f5e9243af0018891079cf7
wangyuxuan2020 <102598771+wangyuxuan2020@users.noreply.github.com>
2022-05-01 13:53:05 +0800

    100——1的倒叙

---
+++ l10.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for (int a=100;a>1;a--)
+	cout<<a<<endl;
+	
+	int c,d,b;
+	cin>>b;
+	c=b%4;
+	if(c==0)
+	{
+		cout<< "ÊÇ";
+	}
+	else
+	{
+		cout<<"·ñ";
+	}
+	
+	
+	return 0;
+}
