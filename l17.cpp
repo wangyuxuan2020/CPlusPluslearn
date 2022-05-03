0c3139bc2fa8ce19b204cdee91f97014b5e3acc0
wangyuxuan2020 <102598771+wangyuxuan2020@users.noreply.github.com>
2022-05-03 12:20:13 +0800

    cos函数，sin函数，tan函数，绝对值，开方的运用

---
+++ l17.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+//	cos函数 
+	double x;
+	x=30;
+	x=cos(x);
+	cout<<x<<endl;
+
+// sin函数	
+	double y;
+	y=30;
+	y=sin(y);
+	cout<<y<<endl;
+
+// tan函数	
+	double z;
+	z=90;
+	z=tan(z);
+	cout<<z<<endl;
+	
+//	绝对值 
+	double a,b,c,e;
+	a=-5,b=6,c=-10;
+	e=fabs(a+b+c);
+	cout<<e<<endl;
+	
+//	开方 
+	double f;
+	f=9;
+	f=sqrt(f);
+	cout<<f;
+	return 0;
+}
