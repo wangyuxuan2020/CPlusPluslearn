a8dfd8f20ec68237ceffcc184ff2107175857f0d
wangyuxuan2020 <102598771+wangyuxuan2020@users.noreply.github.com>
2022-08-09 20:46:02 +0800

    数组

---
+++ 复习2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a[5];
+	for(int c=0;c<5;c++)
+	{
+		scanf("%d",&a[c]);
+	}
+	for(int c=0;c<5;c++)
+	{
+		cout<<a[c];
+	}
+	
+	
+	return 0;
+}
