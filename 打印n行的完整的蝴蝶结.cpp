4eab59ff11831aaac0ef91f016d57d4aba3ffc28
wangyuxuan2020 <102598771+wangyuxuan2020@users.noreply.github.com>
2022-10-23 16:13:47 +0800

    Add files via upload

---
+++ 打印n行的完整的蝴蝶结.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	n=n/2+1;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<'*';
+		}
+		for(int j=1;j<=(n-i)*2;j++)
+		{
+			cout<<' ';
+		}
+		for(int j=1;j<=i;j++)
+		{
+			cout<<'*';
+		}
+		cout<<endl;
+	}
+	for(int i=n-1;i>=1;i--)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<'*';
+		}
+		for(int j=1;j<=(n-i)*2;j++)
+		{
+			cout<<' ';
+		}
+		for(int j=1;j<=i;j++)
+		{
+			cout<<'*';
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
+
---
+++ 沙漏.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	n=n/2+1;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i-1;j++)
+		{
+			cout<<' ';
+		}
+		for(int j=1;j<=(n-i+1)*2-1;j++)
+		{
+			cout<<'*';
+		}
+		cout<<endl; 
+	}
+	for(int i=n-1;i>=1;i--)
+	{
+		for(int j=1;j<=i-1;j++)
+		{
+			cout<<' ';
+		}
+		for(int j=1;j<=(n-i+1)*2-1;j++)
+		{
+			cout<<'*';
+		}
+		cout<<endl; 
+	}
+
+	return 0;
+}
+
